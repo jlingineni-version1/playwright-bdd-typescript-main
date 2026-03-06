063ec3368e7140619f0db24edc19a4643c723978
Jyothi Lingineni <jyothi.lingineni@version1.com>
2026-03-06 10:12:11 +0000

    updated env file pointing to dev and corn schduled at 6pm

--- tests/config/env.ts
+++ tests/config/env.ts
@@ -33,8 +33,8 @@ const ENV_CONFIG: Record<Env, { BASE_URL: string;APP_URL: string;FULLPAGE_URL:st
   },
 };
 
-// Default = test
-const env = (process.env.ENV as Env) || 'test';
+// Default = dev
+const env = (process.env.ENV as Env) || 'dev';
 
 // Export URLs
 export const BASE_URL = ENV_CONFIG[env].BASE_URL;
